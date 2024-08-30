5e3a2c8dbbff750d6a467d20c9d58f9b74fa3933
IJproject <junchan3340@gmail.com>
2024-08-30 17:42:10 +0900

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return (
+    <>
+      <p>test</p>
+      <Button variant="destructive">Click me</Button>
+    </>
+  );
+}
